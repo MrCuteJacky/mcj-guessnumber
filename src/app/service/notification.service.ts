96c3406b4a598a883b34936b8a8dfd4c156d85b0
Jacky <mrcutejacky@gmail.com>
2020-12-15 15:44:03 +0800

    add pwa

--- src/app/service/notification.service.ts
+++ src/app/service/notification.service.ts
@@ -8,11 +8,16 @@ export class NotificationService {
   constructor() { }
 
   register(): void {
-    Notification.requestPermission().then(r => {});
+    Notification.requestPermission().then(r => {
+      console.log(r);
+      this.notify('系統訊息', '註冊通知成功.');
+    });
   }
 
   notify(title: string, body: string): void {
-    new Notification(title, {body: body, icon: 'assets/images/logo.png'});
-    // this.serviceWorkerRegistration.showNotification(title, {body: body, icon: 'assets/images/logo.png'}).then(r => {});
+    console.log('v1');
+    navigator.serviceWorker.getRegistration().then(reg => {
+      reg.showNotification(title, {body: body, icon: 'assets/images/logo.png'}).then();
+    });
   }
 }
