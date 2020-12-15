3329566b637454ab324eaa5a36ae1f78c2b52ef9
Jacky <mrcutejacky@gmail.com>
2020-12-15 16:01:20 +0800

    add pwa

--- src/app/service/notification.service.ts
+++ src/app/service/notification.service.ts
@@ -9,13 +9,11 @@ export class NotificationService {
 
   register(): void {
     Notification.requestPermission().then(r => {
-      console.log(r);
       this.notify('系統訊息', '註冊通知成功.');
     });
   }
 
   notify(title: string, body: string): void {
-    console.log('v1');
     navigator.serviceWorker.getRegistration().then(reg => {
       reg.showNotification(title, {body: body, icon: 'assets/images/logo.png'}).then();
     });
