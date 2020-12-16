48ba0c6357cccc480dea2d32384a1471d16eff84
Jacky <mrcutejacky@gmail.com>
2020-12-16 13:35:50 +0800

    add pwa

--- src/app/service/notification.service.ts
+++ src/app/service/notification.service.ts
@@ -8,14 +8,44 @@ export class NotificationService {
   constructor() { }
 
   register(): void {
-    Notification.requestPermission().then(r => {
+    Notification.requestPermission().then(notificationPermission => {
       this.notify('系統訊息', '註冊通知成功.');
+    });
+
+    navigator.serviceWorker.getRegistration().then(serviceWorkerRegistration => {
+      serviceWorkerRegistration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: this.urlB64ToUint8Array('BGZLRvtyQGmoeKhM38KvOpauQ060CfT8Vb21vg7NEC8JyDvTqWZKIztUD33ebWaF46kyjh6q8L5uOsTq1cuP2LA')
+      }).then(pushSubscription => {
+        console.log(pushSubscription);
+      });
+      serviceWorkerRegistration.addEventListener('push', eventListenerOrEventListenerObject => {
+        console.log('push', eventListenerOrEventListenerObject);
+        // serviceWorkerRegistration.showNotification(eventListenerOrEventListenerObject.data.json().title, eventListenerOrEventListenerObject.data.json());
+      });
     });
   }
 
   notify(title: string, body: string): void {
-    navigator.serviceWorker.getRegistration().then(serviceWorkerRegistration => {
-      serviceWorkerRegistration.showNotification(title, {body: body, icon: 'assets/images/logo.png'}).then();
-    });
+    if (Notification.permission === 'granted') {
+      navigator.serviceWorker.getRegistration().then(serviceWorkerRegistration => {
+        serviceWorkerRegistration.showNotification(title, {body: body, icon: 'assets/images/logo.png'}).then();
+      });
+    }
+  }
+
+  urlB64ToUint8Array(base64String: string): Uint8Array {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
   }
 }
