7dbe16a9e8592aabde028bdcfc62506f78b7b78d
Jacky <mrcutejacky@gmail.com>
2020-12-17 10:56:06 +0800

    add pwa

--- src/app/service/notification.service.ts
+++ src/app/service/notification.service.ts
@@ -1,69 +1,67 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {SwPush} from '@angular/service-worker';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class NotificationService {
 
-  serverPublicKey = 'BLZim1zgHOWGWAEI27u_Detz69HjpxOrSzVH9Rm4us71RuBU_YZ-hoO2mDgLb3cqg9o-HI0WCwtRpWzgShWuyHw';
+    serverPublicKey = 'BLZim1zgHOWGWAEI27u_Detz69HjpxOrSzVH9Rm4us71RuBU_YZ-hoO2mDgLb3cqg9o-HI0WCwtRpWzgShWuyHw';
 
-  constructor(private swPush: SwPush) { }
-
-  register(): void {
-    this.swPush.requestSubscription({
-      serverPublicKey: this.serverPublicKey
-    }).then(pushSubscription => {
-      console.log(JSON.stringify(pushSubscription));
-      this.notify('系統訊息', '註冊通知成功.');
-    }).catch(error => {
-      console.error(error);
-    });
-
-    self.addEventListener('push', (event) => {
-      console.log(event);
-    });
-  }
-
-  notify(title: string, body: string): void {
-    if (Notification.permission === 'granted') {
-      navigator.serviceWorker.getRegistration().then(serviceWorkerRegistration => {
-        // actions?: NotificationAction[];
-        // badge?: string;
-        // body?: string;
-        // data?: any;
-        // dir?: NotificationDirection;
-        // icon?: string;
-        // image?: string;
-        // lang?: string;
-        // renotify?: boolean;
-        // requireInteraction?: boolean;
-        // silent?: boolean;
-        // tag?: string;
-        // timestamp?: number;
-        // vibrate?: VibratePattern;
-        const options: NotificationOptions = {
-          body,
-          icon: 'assets/images/logo.png',
-          timestamp: Date.now()
-        };
-        serviceWorkerRegistration.showNotification(title, options).then();
-      });
+    constructor(private swPush: SwPush) {
     }
-  }
 
-  urlB64ToUint8Array(base64String: string): Uint8Array {
-    const padding = '='.repeat((4 - base64String.length % 4) % 4);
-    const base64 = (base64String + padding)
-        .replace(/\-/g, '+')
-        .replace(/_/g, '/');
-
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
-
-    for (let i = 0; i < rawData.length; ++i) {
-      outputArray[i] = rawData.charCodeAt(i);
+    register(): void {
+        this.swPush.requestSubscription({
+            serverPublicKey: this.serverPublicKey
+        }).then(pushSubscription => {
+            console.log(JSON.stringify(pushSubscription));
+            this.notify('系統訊息', '註冊通知成功v1.');
+        }).catch(error => {
+            console.error(error);
+        });
+        // self.addEventListener('push', event => console.log(event));
     }
-    return outputArray;
-  }
+
+    notify(title: string, body: string): void {
+        if (Notification.permission === 'granted') {
+            navigator.serviceWorker.getRegistration().then(serviceWorkerRegistration => {
+                // actions?: NotificationAction[];
+                // badge?: string;
+                // body?: string;
+                // data?: any;
+                // dir?: NotificationDirection;
+                // icon?: string;
+                // image?: string;
+                // lang?: string;
+                // renotify?: boolean;
+                // requireInteraction?: boolean;
+                // silent?: boolean;
+                // tag?: string;
+                // timestamp?: number;
+                // vibrate?: VibratePattern;
+                const options: NotificationOptions = {
+                    body,
+                    icon: 'assets/images/logo.png',
+                    timestamp: Date.now()
+                };
+                serviceWorkerRegistration.showNotification(title, options).then();
+            });
+        }
+    }
+
+    urlB64ToUint8Array(base64String: string): Uint8Array {
+        const padding = '='.repeat((4 - base64String.length % 4) % 4);
+        const base64 = (base64String + padding)
+            .replace(/\-/g, '+')
+            .replace(/_/g, '/');
+
+        const rawData = window.atob(base64);
+        const outputArray = new Uint8Array(rawData.length);
+
+        for (let i = 0; i < rawData.length; ++i) {
+            outputArray[i] = rawData.charCodeAt(i);
+        }
+        return outputArray;
+    }
 }
